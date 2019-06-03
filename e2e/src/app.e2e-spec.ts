3a2e8ac67e405a182ae1e66ec27e8d218c032890
Daniel <dietrich.daniel1@gmail.com>
2019-06-03 21:07:14 +0200

    add e2e testing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -4,14 +4,25 @@ import { browser, logging } from 'protractor';
 describe('workspace-project App', () => {
   let page: AppPage;
 
+
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to project!');
+  it('GetLoginForm', () => {
+    expect(page.getLoginForm().isPresent()).toBe(true);
   });
+
+  it('getRegButton', () => {
+    expect(page.getRegButton().getText()).toContain('Register');
+  });
+
+
+  it('type stuff into name field', () => {
+    expect(page.login('Tester', 'password').getText()).toContain('dashboard works!');
+  });
+
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,42 @@
-import { browser, by, element } from 'protractor';
+import {$, browser, by, element, protractor} from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
+ /* getTitleText() {
     return element(by.css('app-root h1')).getText() as Promise<string>;
+  }*/
+
+  /*getLoginForm() {
+    return element.all(by.css('app-root app-login')).getText();
+  }*/
+
+  login(title, t2) {
+    const input = element(by.name('username'));
+    input.sendKeys(title);
+    const input2 = element(by.name('password'));
+    input2.sendKeys(t2);
+    browser.sleep(2000);
+    element.all(by.css('.loginButton')).click();
+    browser.sleep(2000);
+    return element(by.tagName('h1'));
   }
+
+  getLoginForm() {
+    return element.all(by.css('.login__wrapper'));
+  }
+
+  getRegButton() {
+    return element.all(by.css('.login__register-link'));
+  }
+
+  send() {
+  }
+ /* getTodoListContent() { return element.all(by.css('app-root app-login')); }
+  getNthTodoTitle(index: number) {
+    const list = this.getTodoListContent();
+    return list.get(index).element(by.css('.title'));
+  }*/
 }
