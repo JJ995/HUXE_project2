e1dd25df08624fe295a23c9c5911de8cd822fd28
Daniel <dietrich.daniel1@gmail.com>
2019-06-07 15:12:46 +0200

    change MovieComponent test

--- src/app/modules/movie/components/movies/movies.component.spec.ts
+++ src/app/modules/movie/components/movies/movies.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MoviesComponent } from './movies.component';
+import {MoviesComponent, MutateMovieListService} from './movies.component';
 import { Apollo } from 'apollo-angular';
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
@@ -9,6 +9,8 @@ import { ToastrModule } from 'ngx-toastr';
 
 describe('MoviesComponent', () => {
   let component: MoviesComponent;
+  let buttonElement: HTMLElement;
+  let button: HTMLElement;
   let fixture: ComponentFixture<MoviesComponent>;
 
   beforeEach(async(() => {
@@ -30,10 +32,28 @@ describe('MoviesComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MoviesComponent);
     component = fixture.componentInstance;
+    buttonElement = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('search window Test', () => {
+    component.searchQuery = 'fight club';
+    component.onSubmit();
+    expect(component.searchActive).toBe(true);
+    component.backToList();
+    expect(component.searchActive).toBe(false);
+  });
+
+  it('show back to my movies button after a search', () => {
+    component.searchQuery = 'fight club';
+    component.onSubmit();
+    expect(component.searchActive).toBe(true);
+    fixture.detectChanges();
+    button = buttonElement.querySelector('button');
+    expect(button.innerText).toContain('Back to favorites');
   });
 });
